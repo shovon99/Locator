6c866bd85d376421244eb371241476aa8ac69113
shovon99 <hasib99@live.com>
2017-10-04 01:47:41 +0600

    Overley Created

--- script.js
+++ script.js
@@ -14,3 +14,12 @@ function showPosition(position) {
     //console.log(position.coords.longitude);
     lng = position.coords.longitude;
 }
+
+/* Open */
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
